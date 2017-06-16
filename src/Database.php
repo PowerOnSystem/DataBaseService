e823df365920140bb905f5d46d2ba333e8c4af72
Lucas Sosa <sosalucas87@gmail.com>
2017-06-16 16:54:47 -0300

    Important changes

--- src/Database.php
+++ src/Database.php
@@ -18,17 +18,65 @@
  */
 
 namespace PowerOn\Database;
+use PowerOn\Exceptions\DevException;
+use PowerOn\Utility\Inflector;
 
 /**
  * Database
  * @author Lucas Sosa
  * @version 0.1
- * @copyright (c) 2016, Lucas Sosa
  */
-class Database {
-
-    public function __construct() {
+class Database extends \mysqli {
+    
+    private $_table_registry = [];
+    
+    /**
+     * Inicializa la configuración de la base de datos
+     * @param string $host Servidor de la base de datos
+     * @param string $user Nombre de usuario
+     * @param string $password Contraseña
+     * @param string $database Base de datos a utilizar
+     * @throws DevException
+     */
+    public function __construct( $host, $user, $password, $database ) {
+        parent::__construct($host, $user, $password, $database);
         
+        if ( $this->connect_errno ) {
+            throw new DevException('Error al conectar la base de datos', [
+                    'mysql_code' => $this->connect_errno, 
+                    'mysql_message'  => $this->connect_error,
+                    'mysql_host' => CNC_DB_HOST, 
+                    'mysql_user' => CNC_DB_USER, 
+                    'mysql_password' => CNC_DB_PSS,
+                    'mysql_database' => $database
+                ]
+            );
+        }
+        if ( !$this->set_charset('utf8') ) {
+            throw new DevException('Error al establecer la codificaci&oacute;n a UTF-8', [
+                    'mysql_code' => $this->errno, 
+                    'mysql_message' => $this->error
+                ]
+            );
+        }
     }
-
+    
+    /**
+     * Devuelve una instancia de la tabla solicitada
+     * @param string $table Nombre de la tabla
+     * @throws DevException
+     * @return Table
+     */
+    public function get($table) {
+        if ( !key_exists($table, $this->_table_registry) ) {
+            $table_class = 'App\Model\Tables\\' . Inflector::classify($table);
+            if ( !class_exists($table_class) ) {
+                throw new DevException(sprintf('No existe la tabla (%s) con la clase (%s)', $table, $table_class));
+            }
+            
+            $this->_table_registry[$table] = new $table_class;
+        }
+        
+        return $this->_table_registry[$table];
+    }
 }
